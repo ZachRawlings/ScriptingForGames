3503fa7f42dfb13f4c7a9f695464f584cc24784c
Zach Rawlings <Zachrg42@gmail.com>
2024-09-13 10:39:03 -0600

    added more functions to the transformcontroller scripts

--- GameObjects AndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjects AndTransformLab/Assets/Scripts/TransformController.cs
@@ -8,10 +8,11 @@ public class TransformController : MonoBehaviour
         {
             //Move the target GameObject
                var x = Mathf.PingPong(Time.time, 3);
-               var p = new Vector3(0, x, 0);
+               var y = Mathf.PingPong(Time.time, 3);
+               var p = new Vector3(y, x, 0);
                transform.position = p;
                
                // Rotate the target GamObject
-               transform.Rotate(new Vector3(30, 30, 30) * Time.deltaTime);
+               transform.Rotate(new Vector3(0, 90, 90) * Time.deltaTime);
          }
 }
